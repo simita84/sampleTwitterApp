89d0592dc387b35cc636192bba35710248f6ed35
Simi Raj <simitresaantony248@gmail.com>
2014-06-17 14:09:20 -0700

    Added title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+
+#Returns full title on a page per tilte basis
+	def full_title(page_title)
+		base_title="Lets Tweet"
+	
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title}|#{page_title}"
+		end
+	
+	end
 end
