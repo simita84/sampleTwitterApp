5bb56f759214b7c40b329839dc74a206e1ea1ffc
Simi Raj <simi.tresa.antony@gmail.com>
2014-06-25 11:06:49 -0700

    Added rember token to attr_accessor  in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name,:password,:password_confirmation
+  attr_accessible :email, :name,:password,:password_confirmation,:remember_token
 
   has_secure_password
 
